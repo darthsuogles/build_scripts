4af875a49ae4d43976c1699ab68f87569abc86f3
Philip Yang <phi@cs.umd.edu>
2014-06-25 11:53:27 -0400

    modified the vtk build scripts

--- vtk/build_vtk.sh
+++ vtk/build_vtk.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-ver_major=5.8
-ver_minor=0
+#ver_major=5.8
+#ver_minor=0
+ver_major=5.10
+ver_minor=1
 ver=$ver_major.$ver_minor
+
+python_ver=${PYTHON_VER%.*} # we only need the major version
 
 # Installation directories
 base_dir=$PWD
-build_dir=/tmp/vtk
+build_basedir=/tmp/vtk
 install_dir=$PWD/$ver
 
 # Prerequisites
 module load qt/4.8.5
+module load python
 
 # Go to the temporary build directory
-mkdir -p $build_dir; cd $build_dir
+mkdir -p $build_basedir; cd $build_basedir
 
 function assert_dir_exist()
 {
@@ -39,29 +44,39 @@ if [ ! -d $ver ]; then
     rm $tarball
 fi
 
+build_dir=$build_basedir/$ver
 cd $ver
-mkdir -p build-tree; mkdir -p $install_dir
-cd build-tree
-cmake -DCMAKE_BUILD_TYPE=None -DCMAKE_INSTALL_PREFIX=$install_dir ..
+#mkdir -p build-tree;
+mkdir -p $install_dir
+#cd build-tree
+#cmake -LAH ..; exit
+
+vtk_pythonpath=$install_dir/lib/python${python_ver}/site-packages
+mkdir -p $vtk_pythonpath
+export PYTHONPATH=$build_dir/Wrapping/Python:$vtk_pythonpath:$PYTHONPATH
+
+cmake -DCMAKE_INSTALL_PREFIX=$install_dir \
+    -DCMAKE_INCLUDE_PATH:PATH=$HOME/local/include \
+    -DCMAKE_LIBRARY_PATH:PATH=$HOME/local/lib \
+    -DCMAKE_BUILD_TYPE:STRING=None \
+    -DCMAKE_C_COMPILER:STRING=gcc \
+    -DCMAKE_CXX_COMPILER:STRING=g++ \
+    -DPYTHON_INCLUDE_DIR:PATH=${PYTHONHOME}/include/python${python_ver} \
+    -DPYTHON_LIBRARY:FILEPATH=${PYTHONHOME}/lib/libpython${python_ver}.so \
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DVTK_USE_TK:BOOL=OFF \
+    -DVTK_USE_QT:BOOL=ON \
+    -DVTK_USE_GUISUPPORT:BOOL=ON \
+    -DVTK_LEGACY_REMOVE:BOOL=ON \
+    -DModule_vtkRenderingMatplotlib:BOOL=ON \
+    -DVTK_INSTALL_PYTHON_MODULE_DIR:PATH=${PYTHONHOME}/lib/python${python_ver}/site-packages \
+    -DVTK_WRAP_PYTHON:BOOL=ON \
+    $PWD
 make -j8
 make install
 
+#-DVTK_USE_GNU_R:BOOL=ON \
+#-DVTK_INSTALL_THIRD_PARTY_LIBRARIES:BOOL=ON \
+
 # Now build the module file
 cd $base_dir
-
-function gen_modules()
-{
-    echo "#%Module 1.0"
-    echo "#"
-    echo "# VTK $ver"
-    echo "#"
-
-    echo "setenv VTK_HOME $install_dir"
-    echo "setenv VTK_VER $ver"
-    if [ -d $install_dir/bin ]; then
-	echo "prepend-path PATH $install_dir/bin"
-    fi
-
-    echo "prepend-path CPATH $install_dir/include/vtk-$ver_major"
-    echo "prepend-path LIBRARY_PATH $install_dir/lib/vtk-$ver_major"
-}
