0e229dc9ca3026d1ebd3204f7337c56326b0e1d9
Philip Yang <phi@cs.umd.edu>
2015-04-04 14:57:41 -0400

    new: eigen fftw

--- opencv/gen_modules_opencv.sh
+++ opencv/gen_modules_opencv.sh
@@ -10,5 +10,5 @@ print_modline "prepend-path PATH $install_dir/bin"
 print_modline "prepend-path CPATH $install_dir/include"
 print_modline "prepend-path LIBRARY_PATH $install_dir/lib"
 print_modline "prepend-path LD_LIBRARY_PATH $install_dir/lib"
-
+print_modline "prepend-path PKG_CONFIG_PATH $install_dir/lib/pkgconfig"
 
