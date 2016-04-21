6893554bf679ef51ba6f0be8064db338bf4ceef4
Philip Yang <phi@quantifind.com>
2016-04-21 14:20:23 -0700

    R and first steps of hosted gcc5

--- R/gen_modules_R.sh
+++ R/gen_modules_R.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-ver=3.1.1
+ver=3.2.5
 
 source ../gen_modules.sh
 
 print_header R $ver
-print_modline "prepend-path PATH $PWD/$ver/bin"
+print_modline "prepend-path PATH ${drgscl_local}/cellar/R/$ver/bin"
 
-man_dir=$(find $PWD/$ver -type d -name man)
+man_dir=$(find ${drgscl_local}/cellar/R/$ver -type d -name man)
 [ -z $man_dir ] || print_modline "prepend-path MANPATH $man_dir"
 
-pkg_config_dir=$(find $PWD/$ver -type d -name pkgconfig)
+pkg_config_dir=$(find ${drgscl_local}/cellar/R/$ver -type d -name pkgconfig)
 [ -z $pkg_config_dir ] || print_modline "prepend-path PKG_CONFIG_PATH $pkg_config_dir"
 
-print_modline "prepend-path LD_LIBRARY_PATH /opt/stow/intel/composer_xe_2013_sp1/mkl/lib/intel64"
+#print_modline "prepend-path LD_LIBRARY_PATH /opt/stow/intel/composer_xe_2013_sp1/mkl/lib/intel64"
