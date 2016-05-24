23198c71cf163dfd86656aab560ecf08b2db68fd
Philip Yang <phi@quantifind.com>
2016-05-24 16:10:29 -0700

    go parallel

--- linuxbrew/build_linuxbrew.sh
+++ linuxbrew/build_linuxbrew.sh
@@ -2,14 +2,13 @@
 
 source ../gen_modules.sh
 
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)" <<EOF
-# \n
-# EOF
-
-(cd $(get_install_root)
-    mkdir -p linuxbrew && cd $_
-    git clone https://github.com/Linuxbrew/linuxbrew.git dev
-)
+# module load git
+# (cd $(get_install_root)
+#     mkdir -p linuxbrew && cd $_
+#     [ -d dev ] || git clone https://github.com/Linuxbrew/linuxbrew.git dev
+#     cd dev && git pull
+#     git submodule update --init --remote --recursive
+# )
 
 guess_print_modfile linuxbrew dev
 print_modline "setenv HOMEBREW_BUILD_FROM_SOURCE yes"
