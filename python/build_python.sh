428bf22aee108555d19cca70ab0c25b0800f1594
Philip Yang <philip@c16-02-24.mtv.quantifind.com>
2016-06-03 13:05:07 -0700

    pre

--- python/build_python.sh
+++ python/build_python.sh
@@ -3,19 +3,18 @@
 source ../build_pkg.sh 
 
 function configure_fn() {
-    module load linuxbrew sqlite
-    brew install sqlite3 bzip2 xz
-
-    local gcc6_flags="-static-libgcc -static-libstdc++"
-    local linuxbrew_flags="-Wl,-rpath=$(brew --prefix)/lib -L$(brew --prefix)/lib"
+    local opt_flags="-O3 -mtune=native -fgraphite -fgraphite-identity -floop-block -floop-interchange -floop-strip-mine -floop-parallelize-all -floop-unroll-and-jam -ftree-loop-linear"
+    local brew_ldflags="-Wl,-rpath=$(brew --prefix)/lib -L$(brew --prefix)/lib -Wl,-rpath=$(brew --prefix)/lib64 -L$(brew --prefix)/lib64"
+    local gcc6_ldflags="-static-libgcc -static-libstdc++"
     ./configure --prefix=${install_dir} \
-	--enable-shared --enable-profiling --with-thread \
-	--enable-loadable-sqlite-extensions \
-	CC=gcc CXX=g++ \
-        CFLAGS="-mtune=native -O3 -g" \
-        CPPFLAGS="-I$(brew --prefix)/include $(pkg-config sqlite3 --cflags)" \
-	LDFLAGS="${gcc6_flags} ${linuxbrew_flags} $(pkg-config sqlite3 --libs) -Wl,-rpath=${install_dir}/lib"
+	            --enable-shared --enable-profiling --with-thread \
+	            --enable-loadable-sqlite-extensions \
+	            CC=gcc CXX=g++ \
+                CFLAGS="-g ${opt_flags}" \
+                CPPFLAGS="-I${SQLITE_ROOT}/include $(pkg-config sqlite3 --cflags)" \
+	            LDFLAGS="${gcc6_ldflags} ${brew_ldflags} $(pkg-config sqlite3 --libs) -Wl,-rpath=${install_dir}/lib"
 }
 
+USE_LATEST_VERSION=no
 guess_build_pkg python http://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz \
     -c "configure_fn" -d "linuxbrew sqlite"
--- tmux/build_tmux.sh
+++ tmux/build_tmux.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 source ../build_pkg.sh
-#source ../gen_modules.sh
 
 ver=2.2
 url=https://github.com/tmux/tmux/releases/download/${ver}/tmux-${ver}.tar.gz
-#module load libevent readline
 guess_build_pkg tmux "${url}" -d "libevent readline"
