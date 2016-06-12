2305990d02f53e077299e9c2541c9a1f81d379cf
Philip Yang <philip@quantifind.com>
2016-06-11 18:29:46 -0700

    cmae

--- cmake/build_cmake.sh
+++ cmake/build_cmake.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 
-BUILD_CMAKE=yes
-
 source ../build_pkg.sh
-source ../gen_modules.sh
 
-#module load toolchain
-#export CC=${TOOLCHAIN_ROOT}/bin/gcc
-#export CXX=${TOOLCHAIN_ROOT}/bin/g++
-
-#export CC=gcc-5
-#export CXX=g++-5
-
-# musl_cross_root="${HOME}/local/musl-cross/x86_64-linux-musl"
-# export CC=${musl_cross_root}/bin/x86_64-linux-musl-gcc
-# export CXX=${musl_cross_root}/bin/x86_64-linux-musl-g++
-# USE_MUSL_CROSS=yes
-
-function build_cmake() {
-    local latest_ver=$(curl -sL https://cmake.org/download/ | \
-                              perl -ne 'print "$1\n" if /cmake-(\d+(\.\d+)*)\.tar\.gz/' | head -n1)
-    [ -z "${latest_ver}" ] || local ver=${latest_ver}
-    local ver_major=${ver%.*}
-    local url="http://www.cmake.org/files/v$ver_major/cmake-$ver.tar.gz"
-    prepare_pkg cmake ${url} ${ver} install_dir
-    [ "yes" == "${USE_MUSL_CROSS}" ] && local install_dir=${install_dir}-musl
-    cmake_ver=${ver}
-    cmake_dir=${install_dir}
-    
-    [ "yes" == "${BUILD_CMAKE}" ] || return
-    cd $ver        
-    ./bootstrap --prefix=${install_dir} \
-                --parallel=32
-    make -j32
-    make install
+function c_fn() {
+    ./bootstrap --prefix="${install_dir}" --parallel=32
 }
-#build_cmake
 
-guess_print_modfile cmake 3.5.2
+USE_LATEST_VERSION=no
+guess_build_pkg cmake https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz -c "c_fn"
