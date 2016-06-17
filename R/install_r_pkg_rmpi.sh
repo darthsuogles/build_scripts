139c1107638fca5cbf6019b3dfffc6a922280744
Philip Yang <philip@quantifind.com>
2016-06-17 14:57:20 -0700

    pre

--- R/install_r_pkg_rmpi.sh
+++ R/install_r_pkg_rmpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 module load openmpi hdf5 szlib
-export LD_LIBRARY_PATH="${HDF5_ROOT}/lib:${OPENMPI_ROOT}/lib:$(brew --prefix)/lib"
+#export LD_LIBRARY_PATH="${HDF5_ROOT}/lib:${OPENMPI_ROOT}/lib:$(brew --prefix)/lib"
 
 _fname=$(mktemp "/tmp/__r_pkg_install.XXXXXXX.R")
 touch ${_fname}
@@ -15,15 +15,9 @@ EOF
 
 cat <<EOF | tee -a "${_fname}"
 install.packages("Rmpi", configure.args=c(
-                        "LDFLAGS='-Wl,-rpath -Wl,${OPENMPI_ROOT}/lib -L${OPENMPI_ROOT}/lib'",
                         "--with-Rmpi-type=OPENMPI", 
                         "--with-Rmpi-libpath=${OPENMPI_ROOT}/lib", 
                         "--with-Rmpi-include=${OPENMPI_ROOT}/include"))
 EOF
 
-cat <<EOF | tee -a "${_fname}"
-install.packages(c("Rcpp", "RcppEigen", "RcppArmadillo", "RInside", "RcppCNPy"))
-EOF
-
 Rscript "${_fname}"
-
