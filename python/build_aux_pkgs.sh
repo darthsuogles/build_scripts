d1b7e58c352202a072c916dcc3e8d9155da85a2e
Philip Yang <philip@quantifind.com>
2016-11-23 11:06:46 -0800

    [AUTO-UPDATE]

--- python/build_aux_pkgs.sh
+++ python/build_aux_pkgs.sh
@@ -1,62 +1,39 @@
 #!/bin/bash
 
+source ../common.sh 
+module load linuxbrew python
+
 # To be called after the new python environment is set
+toolchain='gcc'
+mkl_root=${drgscl_local}/intel/mkl
 
-arch=$PYTHON_ARCH
-ver=$PYTHON_VER
-mkl_root=$PYTHON_MKL_ROOT
-
-echo "installing basic packages"
-./setuptools-0.6c11-py2.7.egg
-
-echo ">> pip"
-cd pip-1.2.1
-python setup.py clean
-python setup.py install
-cd ..
-
-pip install cython
-pip install pillow
-
-if [[ $arch == "intel" ]]; then
-    module load intel
-else
-    module unload intel    
-fi
+pip3 install -U cython pillow psutil line_profiler
 
 echo ">> numpy"
 export LD_LIBRARY_PATH=$mkl_root/lib/intel64:$LD_LIBRARY_PATH
-cd numpy/dev
+
+cat <<_MKL_EOF_ | tee numpy_site.cfg
+[mkl]
+library_dirs = ${mkl_root}/lib/intel64
+include_dirs = ${mkl_root}/include
+rpath = ${mkl_root}/lib/intel64
+mkl_libs = mkl_rt
+lapack_libs = 
+_MKL_EOF_
+
+(cd numpy
 git pull
 cp ../numpy_site.cfg site.cfg
-python setup.py clean
-if [[ $arch == "intel" ]]; then
-    python setup.py config \
+git clean -xdf
+if [[ "${toolchain}" == "intel" ]]; then
+    python3 setup.py config \
 	--compiler=intelem --fcompiler=intelem build_clib \
 	--compiler=intelem --fcompiler=intelem build_ext \
 	--compiler=intelem --fcompiler=intelem install
 else
-    python setup.py config build_clib build_ext install
+    python3 setup.py config build_clib build_ext install
 fi
-cd ../..
+)
 
-# echo ">> scipy"
-# cd scipy/dev
-# git pull
-# python setup.py clean
-# if [[ $arch == "intel" ]]; then
-#     python setup.py config \
-# 	--compiler=intelem --fcompiler=intelem build_clib \
-# 	--compiler=intelem --fcompiler=intelem build_ext \
-# 	--compiler=intelem --fcompiler=intelem install
-# else
-#     python setup.py config build_clib build_ext install
-# fi
-# cd ../..
-pip install scipy
-
-echo "Install the rest"
-pip install ipython
-pip install psutil
-pip install line_profiler
+pip3 install -U scipy ipython pandas statsmodels
 
--- python/build_python.sh
+++ python/build_python.sh
@@ -2,7 +2,6 @@
 
 source ../build_pkg.sh 
 function configure_fn() {
-    #local opt_flags="-O3 -mtune=native -fgraphite -fgraphite-identity -floop-block -floop-interchange -floop-strip-mine -floop-parallelize-all -floop-unroll-and-jam -ftree-loop-linear"
     local opt_flags="-O3 -mtune=native"
     local brew_ldflags="-Wl,-rpath=$(brew --prefix)/lib -L$(brew --prefix)/lib -Wl,-rpath=$(brew --prefix)/lib64 -L$(brew --prefix)/lib64"
     local gcc6_ldflags="-static-libgcc -static-libstdc++"
@@ -15,6 +14,8 @@ function configure_fn() {
 	            LDFLAGS="${gcc6_ldflags} ${brew_ldflags} $(pkg-config sqlite3 --libs) -Wl,-rpath=${install_dir}/lib"
 }
 
+# https://github.com/Linuxbrew/homebrew-core/blob/master/Formula/python3.rb
 USE_LATEST_VERSION=no
-guess_build_pkg python http://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz \
+ver=3.5.2
+guess_build_pkg python "http://www.python.org/ftp/python/${ver}/Python-${ver}.tgz" \
     -c "configure_fn" -d "linuxbrew sqlite"
